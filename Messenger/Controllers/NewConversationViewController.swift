d3cc66149d616532857fe8a26c7575fcab8a5d24
Praveen <praveenmackjr@gmail.com>
2020-11-18 08:59:26 +0530

    Fixed bugs

--- Messenger/Controllers/NewConversationViewController.swift
+++ Messenger/Controllers/NewConversationViewController.swift
@@ -14,6 +14,7 @@ class NewConversationViewController: UIViewController {
     private let spinner = JGProgressHUD(style: .dark)
     
     private var users = [[String: String]]()
+    private var results = [[String: String]]()
     private var hasFetched = false
 
     private let searchBar: UISearchBar = {
@@ -41,6 +42,12 @@ class NewConversationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(noResultLabel)
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         view.backgroundColor = .white
         searchBar.delegate = self
@@ -51,10 +58,35 @@ class NewConversationViewController: UIViewController {
         searchBar.becomeFirstResponder()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+        noResultLabel.frame = CGRect(x: view.width/4, y: (view.height-200)/2, width: view.width/2, height: 200)
+    }
+    
     @objc private func dismissSelf() {
         dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension NewConversationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Start Conversation
+    }
 }
 
 extension NewConversationViewController: UISearchBarDelegate {
@@ -63,6 +95,10 @@ extension NewConversationViewController: UISearchBarDelegate {
         guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
             return
         }
+        
+        searchBar.resignFirstResponder()
+        
+        results.removeAll()
         
         spinner.show(in: view)
         
@@ -80,6 +116,7 @@ extension NewConversationViewController: UISearchBarDelegate {
             DatabaseManager.shared.getAllUsers { [weak self] result in
                 switch result {
                 case .success(let usersCollection):
+                    self?.hasFetched = true
                     self?.users = usersCollection
                     self?.filterUsers(with: query)
                 case .failure(let error):
@@ -96,7 +133,30 @@ extension NewConversationViewController: UISearchBarDelegate {
             return
         }
         
+        self.spinner.dismiss()
         
+        var results: [[String: String]] = self .users.filter({
+            guard let name = $0["name"]?.lowercased() else {
+                return false
+            }
+            
+            return name.hasPrefix(term.lowercased())
+        })
+        
+        self.results = results
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        if results.isEmpty {
+            self.noResultLabel.isHidden = false
+            self.tableView.isHidden = true
+        } else {
+            self.noResultLabel.isHidden = true
+            self.tableView.isHidden = false
+            self.tableView.reloadData()
+        }
     }
     
 }
